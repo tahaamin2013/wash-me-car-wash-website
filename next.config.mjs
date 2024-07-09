7039e47b0495fdebaf1e54e693544a5d52daacc9
tahaamin2013 <tahaamindob2013@gmail.com>
2024-07-10 00:21:53 +0500

    Update the next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['lh3.googleusercontent.com', "raw.githubusercontent.com", "github.com", "gs://washmecarwashlongview-27e04.appspot.com", ""],
+        domains: ['lh3.googleusercontent.com', "raw.githubusercontent.com", "github.com"],
       }
 };
 
