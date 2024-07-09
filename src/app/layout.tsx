08b6b1648db9452a15bb55abf69a6fd9970738be
tahaamin2013 <tahaamindob2013@gmail.com>
2024-07-10 01:37:52 +0500

    fds

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,9 +31,11 @@ export default function RootLayout({
         {children}
         <Topbutton />
         <Footer />
-        <Script>
-          (function(d,u,ac)
-          {var s=d.createElement('script');s.type='text/javascript';s.src='https://a.omappapi.com/app/js/api.min.js';s.async=true;s.dataset.user=u;s.dataset.account=ac;d.getElementsByTagName('head')[0].appendChild(s);})(document,36493,41469);</Script>
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `(function(d,u,ac){var s=d.createElement('script');s.type='text/javascript';s.src='https://a.omappapi.com/app/js/api.min.js';s.async=true;s.dataset.user=u;s.dataset.account=ac;d.getElementsByTagName('head')[0].appendChild(s);})(document,36493,41469);`,
+          }}
+        />
       </body>
     </html>
   );
