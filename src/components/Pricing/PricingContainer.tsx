9cfe28f98c5e10ccc80812842d6a2041f239d551
Muhammad Husnain <aimahusnain@gmail.com>
2024-07-16 22:09:31 +0500

    Pricing section improve too much

---
+++ src/components/Pricing/PricingContainer.tsx
@@ -0,0 +1,90 @@
+import { CldImage } from "next-cloudinary";
+import Image from "next/image";
+import Link from "next/link";
+import { FaCheck } from "react-icons/fa";
+import { Button } from "../ui/button";
+
+const PricingContainer = (data: any) => {
+  const Data = data.data;
+
+  return (
+    <div className="border p-4 bg-white w-full rounded-2xl flex flex-col justify-center items-center text-center sm:w-[400px] px-5 shadow-xl py-12">
+      <CldImage
+        loading="lazy"
+        alt="car image"
+        src={Data.image}
+        width={320}
+        height={250}
+        className="w-52 hover:scale-110 transition-all duration-300"
+      />
+      <h1 className="text-2xl font-bold capitalize">{Data.title}</h1>
+      <h2 className="text-2xl font-bold mt-2 uppercase">
+        hand wash <br /> ONE-TIME
+      </h2>
+      <p className="font-bold text-5xl mb-2 text-black flex items-start justify-center">
+        <span className="text-lg font-bold">$</span>
+        {Data.price}
+      </p>
+      <div className="flex flex-col gap-1 mb-1">
+        {Data.services.map((service: any, index: any) => (
+          <span className="flex items-center justify-center w-full gap-2">
+            <FaCheck fill="green" />
+            {service}
+          </span>
+        ))}
+      </div>
+      <div className="mt-3 border-0 border-t-2 border-black">
+        <div className="font-bold text-lg">
+          <p className="text-[#717c33]">Join</p>
+
+          <h3>Our Monthly Car Wash Membership Includes Teflon Wax</h3>
+          <p className="text-[#717c33]">&</p>
+
+          <span className="text-red-600 text-xl">SAVE MONEY!</span>
+        </div>
+
+        <Link
+          target="_blank"
+          href="https://touch4wash.com/hugesavings/hugesavingmembers.php?flk=2266b543ddd7762a69f0a4c0173dfbdd"
+          className="flex items-center text-xl rounded-md w-full justify-center my-2"
+        >
+          <Button
+            size="lg"
+            aria-label="Compact Club Membership button"
+            className="bg-primaryGreen text-black font-bold py-8 hover:text-white text-lg"
+          >
+            {Data.membership.title} <br />
+            Membership
+          </Button>
+        </Link>
+        <p className="font-bold text-5xl mb-2 text-black flex items-start justify-center">
+          <span className="text-lg font-bold">$</span>
+          {Data.membership.price}
+        </p>
+        <p className="font-sans text-[10px]  uppercase">4 Washes Per Month</p>
+        <p className="flex flex-col gap-1 mb-1 w-full items-center justify-center">
+          Click the Above Link <br />
+          Or
+          <br />
+          Scan the QR Code Below By Utilizing Your Smartphone Camera
+        </p>
+        <div className="w-full flex justify-center">
+          <Image
+            loading="lazy"
+            className="mt-3"
+            src="https://raw.githubusercontent.com/aimahusnain/Washme-CarWash-Images/main/1st%20qr.png"
+            width={150}
+            height={250}
+            alt="Compact Club"
+          />
+        </div>
+        <p>
+          For Instant Access to Our Exclusive Membership Benefits without the
+          need to download the app
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default PricingContainer;
--- src/components/Pricing/index.tsx
+++ src/components/Pricing/index.tsx
@@ -1,75 +1,86 @@
 "use client";
 
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-} from "@/components/ui/carousel";
-import Autoplay from "embla-carousel-autoplay";
-import Image from "next/image";
-import Link from "next/link";
+import { CldImage } from "next-cloudinary";
 import React from "react";
-import { FaCheck } from "react-icons/fa";
-import { Button } from "./ui/button";
-
-interface Plan {
-  name: string;
-  price: string;
-  options: string[];
-  nooptions: string[];
-  minutes: string;
-}
+import PricingContainer from "./PricingContainer";
 
 const Pricing: React.FC = () => {
-  const plugin = React.useRef(
-    Autoplay({ delay: 2000, stopOnInteraction: false })
-  );
+  const pricingData = [
+    {
+      title: "Small Cars",
+      price: "15",
+      image:
+        "https://res.cloudinary.com/dni4hpqo3/image/upload/v1721148674/car3_fksbjh.webp",
+      services: [
+        "Tire and All over Pre-soak",
+        "Tri Color Foam",
+        "Full Body Hand Wash",
+        "High Pressure Dry",
+        "Spot Free Rinse",
+        "Free Vacuum",
+        "Add $2 For Wax",
+      ],
+      membership: {
+        title: "Compact Club",
+        price: "34.95",
+        link: "https://touch4wash.com/hugesavings/hugesavingmembers.php?flk=2266b543ddd7762a69f0a4c0173dfbdd",
+      },
+    },
+    
+    {
+      title: "Mid-Sized Cars",
+      price: "16",
+      image:
+        "https://res.cloudinary.com/dni4hpqo3/image/upload/v1721149659/sedan_n4mh1x.webp",
+      services: [
+        "Tire and All over Pre-soak",
+        "Tri Color Foam",
+        "Full Body Hand Wash",
+        "High Pressure Dry",
+        "Spot Free Rinse",
+        "Free Vacuum",
+        "Add $2 For Wax",
+      ],
+      membership: {
+        title: "Mid Size Club",
+        price: "36.95",
+        link: "https://touch4wash.com/hugesavings/hugesavingmembers.php?flk=d283035156d4b2fd9776ac54c0a05cd7",
+      },
+    },
+  ];
 
   return (
     <div
       id="#MemberShips"
       className="font-sans text-center mt-8 my-8 flex justify-center items-center flex-col"
     >
-      <div className="flex md:flex-row flex-col w-full sm:justify-between justify-center items-center">
-        <div
-          className="md:ml-24"
-          style={{
-            fontSize: "11px",
-            display: "block",
-            textAlign: "center",
-            fontFamily: "arial",
-            width: "205px",
-          }}
-        >
-                <Image
-          src="/0365053m.png"
-          alt="Postcard Advertising"
-          width={300}
-          height={200}
-          layout="responsive"
-          objectFit="cover"
-          className="rounded-lg"
+      <div className="flex flex-wrap sm:justify-start justify-center items-center text-center gap-4 mb-16 w-full">
+        <CldImage
+          src="https://res.cloudinary.com/dni4hpqo3/image/upload/c_scale,f_auto,h_161,q_auto/v1720984049/Wash%20Me%20Car%20Wash%20Images/pricing/0365053m_grpsiy.webp"
+          height={209}
+          width={200}
+          alt="PostCard Wash me car wash image"
+          loading="lazy"
+          className="w-auto h-auto max-w-full"
         />
-        </div>
-        <div className="flex md:flex-row flex-col items-center gap-0 sm:gap-[40px]">
-          <Image
-            loading="lazy"
-            src="https://raw.githubusercontent.com/aimahusnain/Washme-CarWash-Images/main/Frame-1.png"
-            alt="image of house"
-            className="w-[200px] h-[200px] ml-0"
-            width={200}
-            height={200}
-          />
-          <div>
-            <h1 className="text-4xl mb-2 font-bold">Choose Your Plan</h1>
-          </div>
-          <div />
-        </div>
-        <div className="md:mx-20" />
+        <CldImage
+          src="https://res.cloudinary.com/dni4hpqo3/image/upload/c_scale,f_auto,q_auto,w_137/v1720984059/Wash%20Me%20Car%20Wash%20Images/pricing/Frame-1_j60xj5.webp"
+          height={256}
+          width={232}
+          alt="Simonize Double Bond image"
+          loading="lazy"
+          className="w-auto h-auto max-w-full"
+        />
+        <h1 className="font-bold text-3xl sm:text-4xl capitalize text-center sm:text-left">
+          Choose your plan
+        </h1>
       </div>
 
       <div className="flex md:flex-row flex-col w-full items-start mt-4 justify-center gap-[50px] mb-3">
-        <div
+        {pricingData.map((pricing, index) => (
+          <PricingContainer data={pricing} />
+        ))}
+        {/* <div
           className="border p-4 bg-white w-full rounded-2xl flex flex-col justify-center items-center text-center sm:w-[400px] px-5 shadow-xl py-12"
           style={{
             backgroundImage:
@@ -421,7 +432,7 @@ const Pricing: React.FC = () => {
               the need to download the app
             </p>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
