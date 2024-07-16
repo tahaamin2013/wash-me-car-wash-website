e069a36f0b6914dbc5508d43e4186a5f9291b866
Muhammad Husnain <aimahusnain@gmail.com>
2024-07-16 09:58:49 +0500

    Navbar Updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Script from "next/script";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {/* <MyPopup /> */}
-        {/* <Navbar /> */}
+        <Navbar />
         {children}
         {/* <Topbutton /> */}
         {/* <Footer /> */}
