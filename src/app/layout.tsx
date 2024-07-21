aeccbd298f1fe2c2bb8b56432fbc2802fa0bee4b
Muhammad Husnain <aimahusnain@gmail.com>
2024-07-21 17:46:32 +0500

    123

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,12 +25,12 @@ export default function RootLayout({
         {children}
         {/* <Topbutton /> */}
         <Footer />
-        {/* <Script
+        <Script
           id="omappapi-script"
           dangerouslySetInnerHTML={{
             __html: `(function(d,u,ac){var s=d.createElement('script');s.type='text/javascript';s.src='https://a.omappapi.com/app/js/api.min.js';s.async=true;s.dataset.user=u;s.dataset.account=ac;d.getElementsByTagName('head')[0].appendChild(s);})(document,36493,41469);`,
           }}
-        /> */}
+        />
       </body>
     </html>
   );
