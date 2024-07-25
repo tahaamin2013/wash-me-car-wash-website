0e7c1ec264357bebd643c32dd789a4dff92da99a
tahaamin2013 <tahaamindob2013@gmail.com>
2024-07-25 18:16:13 +0500

    fd

--- src/components/WhatWeDo.tsx
+++ src/components/WhatWeDo.tsx
@@ -23,49 +23,39 @@ const WhatWeDo: React.FC = () => {
     setAutoplayEnabled(!autoplayEnabled);
   }, [autoplayEnabled]);
 
-  const carouselItems = useMemo(
-    () =>
-      washingSteps.map((item, index) => (
-        <CarouselItem
-          key={item.title}
-          className="z-10 flex flex-col lg:flex-row justify-center text-left bg-primaryBlue-200 rounded-xl h-[600px] sm:h-[400px] text-white overflow-hidden shadow-xl"
-        >
-          <CldImage
-            width={700}
-            height={500}
-            src={item.Image}
-            alt={`${item.title} image`}
-            className="w-[700px] h-[500px] object-cover object-top lg:block hidden"
-            loading="lazy"
-          />
-          <div className="mt-4 flex flex-col flex-wrap ml-6">
-            <h2 className="text-left text-white font-bold text-2xl">
-              {item.title}
-            </h2>
-            <p className="text-sm mt-3 mb-3">{item.description}</p>
-          </div>
-          <CldImage
-            width={500}
-            height={500}
-            src={item.Image}
-            alt={`${item.title} image`}
-            className="w-screen h-screen object-cover object-top lg:hidden block"
-            loading="lazy"
-          />
-        </CarouselItem>
-      )),
-    []
-  );
+  const carouselItems = useMemo(() => washingSteps.map((item, index) => (
+    <CarouselItem
+      key={item.title}
+      className="z-10 flex flex-col lg:flex-row justify-center text-left bg-primaryBlue-200 rounded-xl h-[600px] sm:h-[400px] text-white overflow-hidden shadow-xl"
+    >
+      <CldImage
+        width={700}
+        height={500}
+        src={item.Image}
+        alt={`${item.title} image`}
+        className="w-[700px] h-[500px] object-cover object-top"
+        loading="lazy"
+      />
+      <div className="mt-4 flex flex-col flex-wrap ml-6">
+        <h2 className="text-left text-white font-bold text-2xl">
+          {item.title}
+        </h2>
+        <p className="text-sm mt-3 mb-3">{item.description}</p>
+      </div>
+    </CarouselItem>
+  )), []);
 
   return (
     <section
       id="Our Services"
       className="flex mx-7 flex-col justify-center items-center my-5"
     >
-      <p className="tracking-widest text-blue">WHAT WE DO</p>
-      <h1 className="text-2xl mb-2 sm:text-3xl font-bold">
-        Step-by-Step Guide to Washing Success
-      </h1>
+      <div className="text-center flex flex-col gap-2 mb-2">
+        <p className="tracking-widest text-blue">WHAT WE DO</p>
+        <h1 className="text-2xl sm:text-3xl font-bold">
+          Step-by-Step Guide to Washing Success
+        </h1>
+      </div>
 
       <Carousel
         plugins={[plugin.current]}
@@ -73,7 +63,9 @@ const WhatWeDo: React.FC = () => {
         onMouseEnter={toggleAutoplay}
         onMouseLeave={toggleAutoplay}
       >
-        <CarouselContent>{carouselItems}</CarouselContent>
+        <CarouselContent>
+          {carouselItems}
+        </CarouselContent>
         <CarouselPrevious />
         <CarouselNext />
       </Carousel>
