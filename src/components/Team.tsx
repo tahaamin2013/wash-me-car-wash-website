be9ec979f00547b2436e6f36ee76708f1fa95e21
tahaamin2013 <tahaamindob2013@gmail.com>
2024-07-27 10:49:56 +0500

    fds

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -5,15 +5,23 @@ import Image from "next/image";
 const Team = () => {
   return (
     <div id="Meet the Team" className="relative overflow-x-hidden">
-      <div className="h-[520px]">
+      <div className="h-[460px]">
         <Image
           alt="Wash me car wash team image"
           src="/Team.webp"
-          width={700}
-          height={510}
+          fill
           loading="lazy"
-          className="object-cover w-fit sm:w-screen h-fit sm:h-[510px]  absolute inset-0 object-center "
+          className="object-cover sm:block hidden  absolute inset-0 object-center "
         />
+        <Image
+          alt="Wash me car wash team image"
+          src="/MobileTeam.jpg  "
+          width={500}
+          height={500}
+          loading="lazy"
+          className="object-cover sm:hidden block  absolute inset-0 object-center "
+        />
+
       </div>
       <div className="absolute border-2 sm:border-0  sm:border-none  bottom-0 left-0 right-auto sm:left-5 sm:right-0 sm:top-auto sm:bottom-5 rounded-none w-full sm:w-[400px] h-fit text-left text-black transition-all bg-white p-7">
         <h3 className="text-2xl font-bold mb-3 text-primaryBlue-200">
