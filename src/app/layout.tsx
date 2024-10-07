365dd5a41b3b08c687a1ad6794aa2c11804e90ee
tahaamin2013 <tahaamindob2013@gmail.com>
2024-10-08 01:04:44 +0500

    fds

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9571755808195636"
+          crossOrigin="anonymous"></script>
+      </head>
       <body className={`${inter.className}`}>
         <Navbar />
         {children}
